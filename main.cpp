7029155236ae559d1c912e1726e8d02b9b9a59f1
msmhi52 <50022048+msmhi52@users.noreply.github.com>
2020-03-03 22:00:10 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char** argv){
     try{
          PercolationStats pstats(atoi(argv[1]), atoi(argv[2]));
-         cout << "mean  =  " << pstats.mean() << "\n" << "stddev  =  " << pstats.stddev() << "\n" << "95%% confidence interval  =  [" << 
+         cout << "mean  =  " << pstats.mean() << "\n" << "stddev  =  " << pstats.stddev() << "\n" << "95% confidence interval  =  [" << 
              pstats.confidenceLo() << ", " << pstats.confidenceHi() << "]\n";
      }
      catch(const out_of_range& oor){
